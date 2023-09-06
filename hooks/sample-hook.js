4c6e20e41c5eb323914be705b5bbee35ea1501ae
Roman Kukharchuk <kukharchuk.roman@gmail.com>
2023-09-06 17:01:53 +0200

    Updated SDK version

--- hooks/sample-hook.js
+++ hooks/sample-hook.js
@@ -1,3 +1,10 @@
+const NormalSdk = require("@normalframework/applications-sdk");
+
+/**
+ * Invoke hook function
+ * @param {NormalSdk.InvokeParams} params
+ * @returns {NormalSdk.InvokeResult}
+ */
 module.exports = ({ points, sdk }) => {
   console.log("runnning sample hook!");
 };
